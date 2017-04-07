b941549660c7009b685445c53aacf2f7adfec578
RMJ <register@fotonstudios.com>
2017-04-07 16:37:23 -0400

    Update column widths for template

--- template-web-component.php
+++ template-web-component.php
@@ -17,12 +17,12 @@
                 <h1><?php the_title(); ?></h1>
                 
                 <div class="row">
-                  <div class="col-xs-12 col-sm-9">
+                  <div class="col-xs-12 col-sm-8 col-md-9">
                     <?php while ( have_posts() ) : the_post(); ?>
                       <?php the_content(); ?>
                     <?php endwhile; // end of the loop. ?>
                   </div>
-                  <div class="col-xs-12 col-sm-3">
+                  <div class="col-xs-12 col-sm-4 col-md-3">
                     <div class="wp_responsive_doc wp_responsive_doc_toc">
                       <h2>Table of Contents</h2>
                       <?php $associated_elements = get_field('associated_elements'); ?>
