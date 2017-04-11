d9dc3172f8b860afe1cb2e6ea30744defa48c1e1
RMJ <register@fotonstudios.com>
2017-04-11 09:56:22 -0400

    markup modal

--- template-web-component.php
+++ template-web-component.php
@@ -368,9 +368,7 @@
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
       </div>
       <div class="modal-body">
-        <pre class="modal_markup">
-
-        </pre>
+        <pre class="modal_markup"></pre>
       </div>
     </div>
   </div>
