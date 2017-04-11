393bb06e4c71fb7a29472532be0186426ed925ba
RMJ <register@fotonstudios.com>
2017-04-11 10:49:14 -0400

    responsive test template

---
+++ single-element.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The template for displaying all single Element posts.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package responsive-documentation
+ */
+?>
+<?php get_header(); ?>
+
+    <div class="row-offcanvas row-offcanvas-left">
+      <div class="content_container subpage component_template">
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-12">
+
+asdf
+              
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sidebar-offcanvas" id="sidebar" role="navigation">
+        <nav class="navbar navbar-default" role="navigation">
+          <div class="collapse navbar-collapse no-transition" id="bs-example-navbar-collapse-1">
+            <?php wp_nav_menu( array('menu' => 'TopNav')); ?>
+          </div>
+        </nav>
+      </div>
+      <div class="clearfix"></div>
+      <?php get_footer(); ?>
+    </div>
+    
+  </div><!-- #content -->
+
+</div><!-- #page -->
+
+</body>
+</html>
