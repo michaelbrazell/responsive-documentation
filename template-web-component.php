62e78b22b1f7edc0bd0c28975cfb91430d539074
RMJ <register@fotonstudios.com>
2017-04-11 09:37:08 -0400

    markup modal

--- template-web-component.php
+++ template-web-component.php
@@ -383,7 +383,7 @@ $('#component_markup_modal').on('show.bs.modal', function (event) {
   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
   // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
   var modal = $(this)
-  modal.find('.modal_title').text(recipient_title)
+  modal.find('.modal_title').html(recipient_title)
 })                
   
 })(jQuery); 
