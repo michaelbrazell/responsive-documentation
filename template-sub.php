b100fd8f2ca6c54b242697ef258cf1d157c181a7
scribbins <register@fotonstudios.com>
2017-04-24 09:51:26 -0400

    sub templates updates

--- template-sub.php
+++ template-sub.php
@@ -14,6 +14,7 @@
             <div class="col-xs-12">
               <div class="wp_responsive_doc">
                 <?php while ( have_posts() ) : the_post(); ?>
+                  <h1><?php the_title(); ?></h1>
                   <?php the_content(); ?>
                 <?php endwhile; // end of the loop. ?>
               </div>
