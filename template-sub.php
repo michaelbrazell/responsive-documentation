fa470189b843eab5aa2621875bdac9156f406243
scribbins <register@fotonstudios.com>
2017-04-24 09:49:14 -0400

    changes to sub template

--- template-sub.php
+++ template-sub.php
@@ -12,15 +12,17 @@
         <div class="container">
           <div class="row">
             <div class="col-xs-12">
-              <?php while ( have_posts() ) : the_post(); ?>
-              <?php get_template_part( 'template-parts/content', 'page' ); ?>
-              <?php
-                    // If comments are open or we have at least one comment, load up the comment template
-                    if ( comments_open() || get_comments_number() ) :
-                      comments_template();
-                    endif;
-                  ?>
-              <?php endwhile; // end of the loop. ?>
+              <div class="wp_responsive_doc">
+                <?php while ( have_posts() ) : the_post(); ?>
+                <?php get_template_part( 'template-parts/content', 'page' ); ?>
+                <?php
+                      // If comments are open or we have at least one comment, load up the comment template
+                      if ( comments_open() || get_comments_number() ) :
+                        comments_template();
+                      endif;
+                    ?>
+                <?php endwhile; // end of the loop. ?>
+              </div>
             </div>
           </div>
         </div>
