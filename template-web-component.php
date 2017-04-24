3c88c1c9265fa9f6ca1db0e638e07d0b5c6038b8
scribbins <register@fotonstudios.com>
2017-04-24 11:09:22 -0400

    set grids on all components

--- template-web-component.php
+++ template-web-component.php
@@ -122,7 +122,7 @@
                     
                       <div style="height:200px; background: red;"></div>
 
-                    <?
+                    <?php
                       if ($interactive_example_count == 1) {
                         $interactive_example_grid = "col-xs-12";
                       } elseif ($interactive_example_count == 2) {                    
