4d4fac503ff1579afbda0412b40f76737a3a0775
michaelbrazell <mike.murray@mathworks.com>
2018-01-26 16:22:40 -0500

    Modify CORS to allow any GET but not POST or DELETE

--- functions.php
+++ functions.php
@@ -213,7 +213,17 @@ add_filter( 'searchwp_custom_fields', 'my_searchwp_custom_fields', 10, 3 );
   * Enable WP-API CORS
 	*/
 
-add_filter( 'allowed_http_origin', '__return_true' );
+function allow_get_over_cors() {
+	remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
+	add_filter( 'rest_pre_serve_request', function( $value ) {
+		header( 'Access-Control-Allow-Origin: *' );
+		header( 'Access-Control-Allow-Methods: GET' );
+		header( 'Access-Control-Allow-Credentials: true' );
+		header( 'Access-Control-Expose-Headers: Link', false );
+		return $value;
+	} );
+}
+add_action( 'rest_api_init', 'allow_get_over_cors', 15 );
 
 
 
