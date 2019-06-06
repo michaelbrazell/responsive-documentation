90d1ff060fe91b373a6ac85b35673072c2821143
scribbins <register@fotonstudios.com>
2019-06-06 10:07:45 -0400

    ifame Templae for Standards

---
+++ template-iframe.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Template Name: iframe-Template
+ *
+ * @package responsive-documentation
+ */
+?>
+<?php while ( have_posts() ) : the_post(); ?>
+<?php the_content(); ?>
+<?php endwhile; // end of the loop. ?>
