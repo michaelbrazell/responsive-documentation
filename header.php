a9e702927fdf2a237435e99a44713de1ec5264c2
scribbins <register@fotonstudios.com>
2020-07-30 17:18:39 -0400

    Update header.php

--- header.php
+++ header.php
@@ -21,7 +21,6 @@ $ic_track_mini = rtrim($ic_track_mini, ".mathworks.com");
 <html <?php language_attributes(); ?>><head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="Content-Security-Policy" content="default-src 'self'">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <?php wp_head(); ?>
